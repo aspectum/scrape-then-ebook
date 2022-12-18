0cc5c7d139a121c161099fe4caa85ce65aafca7a
Pedro Garcia <drogpe@gmail.com>
2022-12-18 10:19:30 -0300

    auto fetch cover and metadata and accept link via argv

--- src/make-ebook.sh
+++ src/make-ebook.sh
@@ -1,1 +1,1 @@
-pandoc -f html -t epub --epub-metadata src/metadata.xml --toc --epub-cover-image=output/cover.jpg --resource-path=output --css=src/book.css -o output/wcss.epub output/clean.html
+pandoc -f html -t epub --epub-metadata output/metadata.xml --toc --epub-cover-image=output/cover.jpg --resource-path=output --css=src/book.css -o output/wcss.epub output/clean.html
--- src/scrape.ts
+++ src/scrape.ts
@@ -1,16 +1,24 @@
 import fs from 'fs';
 import path from 'path';
+import { downloadImage } from './get-images';
 import { fetchAndParseHTML, scrapeParseAndSave } from './utils';
 
-const MAIN_URL = process.env.MAIN_URL as string;
 const BASE_URL = process.env.BASE_URL as string;
+const MAIN_URL = process.argv[2]
+  ? BASE_URL + '/fiction/' + process.argv[2].match(/\d/g)?.join('')
+  : (process.env.MAIN_URL as string);
 const buildUrl = (path: string) => BASE_URL + path;
 
-const rootdir = path.join(process.cwd(), 'output', 'chapters');
+const rootdir = path.join(process.cwd(), 'output');
 if (fs.existsSync(rootdir)) {
   fs.rmSync(rootdir, { recursive: true, force: true });
 }
 fs.mkdirSync(rootdir, { recursive: true });
+
+const chaptersdir = path.join(rootdir, 'chapters');
+fs.mkdirSync(chaptersdir, { recursive: true });
+
+console.log(`Scraping ${MAIN_URL}`);
 
 const doc = await fetchAndParseHTML(MAIN_URL);
 
@@ -21,5 +29,32 @@ console.log(`${chapters.length} chapters`);
 chapters.forEach(async (chapter, chapIndex) => {
   const link = chapter.querySelector('a')!.href;
   console.log(`Processing chapter ${chapIndex + 1}`);
-  scrapeParseAndSave(buildUrl(link), path.join(rootdir, `${chapIndex + 1}.html`));
+  scrapeParseAndSave(buildUrl(link), path.join(chaptersdir, `${chapIndex + 1}.html`));
 });
+
+// get metadata
+
+console.log(`Getting metadata`);
+
+const title = doc.querySelector('h1[property=name]')?.textContent ?? 'No Title';
+const author =
+  doc.querySelector('h4[property=author] span[property=name] a')?.textContent ?? 'No Author';
+
+const metadata = `<dc:title>${title}</dc:title>
+<dc:language>en-US</dc:language>
+<dc:creator>${author}</dc:creator>
+<dc:publisher>RoyalRoad</dc:publisher>`;
+
+fs.writeFileSync(path.join(rootdir, 'metadata.xml'), metadata);
+
+// get cover
+
+console.log(`Getting cover`);
+
+const coverUrl = doc
+  .querySelector<HTMLImageElement>('.cover-art-container img')
+  ?.src?.replace('covers-full', 'covers-large');
+console.log(coverUrl);
+if (coverUrl) {
+  downloadImage(coverUrl, path.join(rootdir, 'cover.jpg'));
+}
