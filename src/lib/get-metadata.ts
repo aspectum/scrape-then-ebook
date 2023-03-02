3c259f2a40c4b4f3d2bb96a8807e6e1e99df6642
Pedro Garcia <drogpe@gmail.com>
2023-03-01 23:13:55 -0300

    separate metadata logic

---
+++ src/lib/get-metadata.ts
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import dedent from 'ts-dedent';
+
+export const getMetadata = (doc: Document, rootdir: string) => {
+  console.log(`Getting metadata`);
+
+  const title = doc.querySelector('h1[property=name]')?.textContent ?? 'No Title';
+  const author =
+    doc.querySelector('h4[property=author] span[property=name] a')?.textContent ?? 'No Author';
+
+  const metadata = dedent`<dc:title>${title}</dc:title>
+                          <dc:language>en-US</dc:language>
+                          <dc:creator>${author}</dc:creator>
+                          <dc:publisher>RoyalRoad</dc:publisher>`;
+
+  fs.writeFileSync(path.join(rootdir, 'metadata.xml'), metadata);
+};
--- src/scrape.ts
+++ src/scrape.ts
@@ -1,7 +1,7 @@
-import fs from 'fs';
 import path from 'path';
 import { cli, createDirectories, fetchAndParseHTML, scrapeParseAndSave } from './lib';
 import { downloadImage } from './lib/download-image';
+import { getMetadata } from './lib/get-metadata';
 
 const MAIN_URL = cli.input[0];
 const BASE_URL = new URL(MAIN_URL).origin;
@@ -34,20 +34,7 @@ chapters.slice(STARTING_CHAPTER - 1, ENDING_CHAPTER).forEach(async (chapter, cha
   scrapeParseAndSave(buildUrl(link), path.join(chaptersdir, `${chapIndex + 1}.html`));
 });
 
-// get metadata
-
-console.log(`Getting metadata`);
-
-const title = doc.querySelector('h1[property=name]')?.textContent ?? 'No Title';
-const author =
-  doc.querySelector('h4[property=author] span[property=name] a')?.textContent ?? 'No Author';
-
-const metadata = `<dc:title>${title}</dc:title>
-<dc:language>en-US</dc:language>
-<dc:creator>${author}</dc:creator>
-<dc:publisher>RoyalRoad</dc:publisher>`;
-
-fs.writeFileSync(path.join(rootdir, 'metadata.xml'), metadata);
+getMetadata(doc, rootdir);
 
 // get cover
 
